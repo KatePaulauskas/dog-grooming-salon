f7601e92e22a53ac2b8a693f514acd79145e5c3e
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-05-23 17:05:54 +0000

    Create About and Services Models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class About(models.Model):
+    image = models.ImageField
+    welcome_message = title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+class Services(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.DurationField()
