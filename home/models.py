64862c8eaf442d4b8c035d86d8b975e20cf26f05
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-05-23 17:41:39 +0000

    Update database with About and Services models

--- home/models.py
+++ home/models.py
@@ -9,9 +9,18 @@ class About(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
 
+# Add Meta class to the models to avoid 's' 
+# Being appended at the end of the model name in the Django admin panel
+# Source: https://docs.djangoproject.com/en/5.0/ref/models/options/#verbose-name-plural
+    class Meta:
+        verbose_name_plural = "About"
+
 class Services(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     duration = models.DurationField()
+
+    class Meta:
+        verbose_name_plural = "Services"
