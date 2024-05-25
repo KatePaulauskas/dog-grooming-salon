fbd05a2e9640355647f371c57f5eef6a4428b00a
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-05-25 09:08:40 +0000

    Install Cloudinary, update models for image serving

--- home/models.py
+++ home/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 
 class About(models.Model):
-    image = models.ImageField
+    image = CloudinaryField('image', default='placeholder')
     welcome_message = title = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -17,6 +19,7 @@ class About(models.Model):
 
 class Services(models.Model):
     id = models.AutoField(primary_key=True)
+    service_image = CloudinaryField('image', default='placeholder')
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
