940df17f0b46a0ef74b5cd1664c289005cf0206f
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-06-03 12:23:19 +0000

    Update home view to display About and Services models

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import About, Services
 
-
-# Create your views here.
-
-
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Services
     template_name = 'home/index.html'
+    context_object_name = 'services_list'
 
+    # Use the get_context_data method to include multiple models' data in a Django class-based view
+    # https://docs.djangoproject.com/en/5.0/topics/class-based-views/generic-display/#adding-extra-context
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the About model data above services list
+        context['about'] = About.objects.first()
+        return context
