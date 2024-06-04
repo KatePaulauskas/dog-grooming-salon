0c2e1aa3847e4d39517c39bfac180b9cda774e6a
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-06-04 12:28:35 +0000

    Create Gallery app and model, configurate admin panel

---
+++ gallery/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from cloudinary.models import CloudinaryField
+
+
+# Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=200)
+    updated_on = models.DateTimeField(auto_now=True)
+    image = CloudinaryField('image', default='placeholder')
+
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Gallery"
