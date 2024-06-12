b0e7fdc66d22af649b570f2b0e2c17681857a5d1
Kate Paulauskas <ekaterina.paulauskas@gmail.com>
2024-06-12 18:27:44 +0000

    Setup appointment view, URL, and booking button link

--- appointment/urls.py
+++ appointment/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.appointment, name='appointment'),
+    path('', views.book_appointment, name='appointment'),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .forms import AppointmentForm
 
-# Create your views here.
+def book_appointment(request):
+    appointment_form = AppointmentForm()
+
+    return render(
+        request,
+        "appointment/appointment.html",
+        {
+            "appointment_form": appointment_form,
+        },
+    )
