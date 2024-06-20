2939fa87d017c4232778e2913c0fae79614f2ec5
Ekaterina Mikheeva <ekaterina.paulauskas@gmail.com>
2024-06-20 20:22:00 +0000

    Split booking into two steps, add step one HTML

--- appointment/forms.py
+++ appointment/forms.py
@@ -4,21 +4,16 @@ import datetime
 
 # Form for booking appointments with dynamic time slot choices.
 
-class AppointmentForm(forms.ModelForm):
+class StepOneForm(forms.ModelForm):
     """
     Specifies how date and time fields should be handled within the form 
-    Sets date field with Calendar widget
+    Sets date field with Calendar widget.
     Sources: 
     https://pythonassets.com/posts/date-field-with-calendar-widget-in-django-forms/
     https://docs.djangoproject.com/en/5.0/ref/forms/fields/
     """
     date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    time = forms.ChoiceField(choices=[])
 
     class Meta:
         model = Appointment
-        fields = ('service', 'groomer', 'date', 'time')
-    
-    # Updates the 'time' field choices with available time slots.
-    def set_time_choices(self, choices):
-        self.fields['time'].choices = choices
+        fields = ('service', 'groomer', 'date',)
